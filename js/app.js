666e720509ccaa516ff197f95ed6d49ddb7f9fc3
Terry Parrish <tparri2@emory.edu>
2020-04-14 19:53:08 -0400

    refactored

--- js/app.js
+++ js/app.js
@@ -17,75 +17,69 @@
  * Define Global Variables
  * 
 */
-let navBarList = document.getElementById("navbar__list");
-const landingContainerCount = document.getElementsByClassName("landing__container").length;
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-let isInViewport = function(elem) {
-    let bounding = elem.getBoundingClientRect();
-    return (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
+// init the nav
+function initNav() {
+  let isInViewport = function(elem) {
+      let bounding = elem.getBoundingClientRect();
+      return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
 
-// build the nav
-function buildNav() {
-    for (let i = 1; i < landingContainerCount + 1; i++) {
-        const id = "section" + i;
-        let section = document.querySelector("#"+id);
-        let sectionTitle = section.getAttribute('data-nav');
-        let selectionListItem = buildListItem(id, sectionTitle);
-        
-        // Scroll to section on link click
-        // Scroll to anchor ID using scrollTO event
-        selectionListItem.addEventListener("click", function() {
-            section.scrollIntoView ({
-            behavior: 'smooth'}
-          )
-        });
-    
-        // Set sections as active
-        // Add class 'active' to section when near top of viewport
-        window.addEventListener(
-          "scroll",
-          function(event) {
-            let nItem = this.document.getElementById('nav-'+id);
-            if (isInViewport(section)) {
-              section.classList.add("your-active-class");
-              nItem.classList.add("link__active");  
-            } else {
-              section.classList.remove("your-active-class");
-              nItem.classList.remove("link__active");
-            }
+  const nav = document.querySelector('#navbar__list');
+  const sections = document.querySelectorAll('section');
+  let firstLink = true;
+  for (let section of sections) {
+      const navLink = document.createElement('li');
+      if(firstLink) {
+        firstLink = false;
+        navLink.innerHTML =
+        `<a href="#${section.id}" class="menu__link link__active" data-link="${section.dataset.nav}">
+            ${section.dataset.nav}
+        </a>`
+      } else {
+        navLink.innerHTML =
+        `<a href="#${section.id}" class="menu__link" data-link="${section.dataset.nav}">
+            ${section.dataset.nav}
+        </a>`
+      }
+      nav.appendChild(navLink);
+      
+      window.addEventListener(
+        "scroll",
+        function(event) {
+          const link = document.querySelectorAll(`[data-link="${section.dataset.nav}"]`)[0];
+          if (isInViewport(section)) {
+            section.classList.add("your-active-class");
+            link.classList.add("link__active");
+          } else {
+            section.classList.remove("your-active-class");
+            link.classList.remove("link__active"); 
           }
-        );
-
-        navBarList.appendChild(selectionListItem);
-    }
+        }
+     );
+     
+  }
 }
 
-// Build menu 
-function buildListItem(id, sectionTitle) {
-    let selectionListItem = document.createElement('li');
-    selectionListItem.id = 'nav-'+id;
-    selectionListItem.className = 'menu__link';
-    selectionListItem.textContent = sectionTitle;
-    return selectionListItem;
-}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-buildNav();
+
+initNav();
+
 /**
  * End Main Functions
  * Begin Events
