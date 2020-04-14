99e90965cfdd117c99d5060d5257be5a1b9c7cb7
Terry Parrish <tparri2@emory.edu>
2020-04-14 12:25:45 -0400

    adding fixing from review

--- README.md
+++ README.md
@@ -6,9 +6,8 @@
 * [Instructions](#instructions)
 
 ## Description
+The code in this project is meant to satisfy the project # 2 rubric of the Front Ent Web Developer nanodegree. Landing page is build on using HTML, CSS and Vanilla JavaScript. The goal of this project is to make the HTML elements dynamicity via JavaScript
 
-This project is build on using HTML, CSS and Vanilla JavaScript.
-It implements DOM manipulation to toggle active classes on HTML elements.
 
 ## Instructions
 
--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@
 */
 let navBarList = document.getElementById("navbar__list");
 const landingContainerCount = document.getElementsByClassName("landing__container").length;
+
 /**
  * End Global Variables
  * Start Helper Functions
@@ -27,20 +28,20 @@ const landingContainerCount = document.getElementsByClassName("landing__containe
 let isInViewport = function(elem) {
     let bounding = elem.getBoundingClientRect();
     return (
-        bounding.top <= 50 &&
-        bounding.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <=
-        (window.innerWidth || document.documentElement.clientWidth)
-    );
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 };
 
 // build the nav
 function buildNav() {
-    for (i = 1; i < landingContainerCount + 1; i++) {
-        let section = document.querySelector("#section" + i);
+    for (let i = 1; i < landingContainerCount + 1; i++) {
+        const id = "section" + i;
+        let section = document.querySelector("#"+id);
         let sectionTitle = section.getAttribute('data-nav');
-        let selectionListItem = buildListItem(sectionTitle);
+        let selectionListItem = buildListItem(id, sectionTitle);
         
         // Scroll to section on link click
         // Scroll to anchor ID using scrollTO event
@@ -53,35 +54,38 @@ function buildNav() {
         // Set sections as active
         // Add class 'active' to section when near top of viewport
         window.addEventListener(
-            "scroll",
-            function(event) {
-              if (isInViewport(section)) {
-                section.classList.add("your-active-class");
-              } else {
-                section.classList.remove("your-active-class");
-              }
-            },
-            false
+          "scroll",
+          function(event) {
+            let nItem = this.document.getElementById('nav-'+id);
+            if (isInViewport(section)) {
+              section.classList.add("your-active-class");
+              nItem.classList.add("link__active");  
+            } else {
+              section.classList.remove("your-active-class");
+              nItem.classList.remove("link__active");
+            }
+          }
         );
-    
+
         navBarList.appendChild(selectionListItem);
     }
 }
 
 // Build menu 
-function buildListItem(sectionTitle) {
+function buildListItem(id, sectionTitle) {
+    console.log("id => " + id);
     let selectionListItem = document.createElement('li');
+    selectionListItem.id = 'nav-'+id;
     selectionListItem.className = 'menu__link';
     selectionListItem.textContent = sectionTitle;
-
     return selectionListItem;
 }
+
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-
 buildNav();
 /**
  * End Main Functions
