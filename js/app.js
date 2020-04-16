69a4c87be694c44cd9f5f20701148a9496b65ad4
Terry Parrish <tparri2@emory.edu>
2020-04-16 16:14:03 -0400

    address review comments

--- js/app.js
+++ js/app.js
@@ -23,65 +23,111 @@
  * Start Helper Functions
  * 
 */
-// init the nav
-function initNav() {
-  let isInViewport = function(elem) {
-      let bounding = elem.getBoundingClientRect();
-      return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  };
-
-  const nav = document.querySelector('#navbar__list');
-  const sections = document.querySelectorAll('section');
-  let firstLink = true;
-  for (let section of sections) {
-      const navLink = document.createElement('li');
-      if(firstLink) {
-        firstLink = false;
-        navLink.innerHTML =
-        `<a href="#${section.id}" class="menu__link link__active" data-link="${section.dataset.nav}">
-            ${section.dataset.nav}
-        </a>`
-      } else {
-        navLink.innerHTML =
-        `<a href="#${section.id}" class="menu__link" data-link="${section.dataset.nav}">
-            ${section.dataset.nav}
-        </a>`
-      }
-      nav.appendChild(navLink);
-      
-      window.addEventListener(
-        "scroll",
-        function(event) {
-          const link = document.querySelectorAll(`[data-link="${section.dataset.nav}"]`)[0];
-          if (isInViewport(section)) {
-            section.classList.add("your-active-class");
-            link.classList.add("link__active");
-          } else {
-            section.classList.remove("your-active-class");
-            link.classList.remove("link__active"); 
-          }
-        }
-     );
-     
-  }
+const createNavItem = (section)=> { 
+  const navItem = document.createElement('li');
+  navItem.innerText = `${section.dataset.nav}`;
+  navItem.classList.add('menu__link');
+  navItem.setAttribute('data-link', `${section.dataset.nav}`);
+  return navItem;
 }
 
+const sectionCoord = (section) => {
+  let bodyTop = document.body.getBoundingClientRect().top;
+  let sectionTop = section.getBoundingClientRect().top;
+  let secCoord = sectionTop - bodyTop;
+  return secCoord; 
+}
+
+const getSectionCoords = () => {
+  const sections = document.querySelectorAll("section");
+  const sectionTop = [];
+
+  for (section of sections){
+    sectionTop.push(sectionCoord(section));
+  }
+  return sectionTop;
+}
+
+const scrollEventHandler = ()=> {
+  const sectionCoords = getSectionCoords();
+
+  for(let i = 0; i < sectionCoords.length; i++){
+    const sections = document.querySelectorAll("section");
+    const navItems = document.querySelectorAll(".menu__link");
+
+    if(window.scrollY >= sectionCoords[i] && 
+      !(window.scrollY > sectionCoords[i+1])) {
+      sections[i].classList.add("your-active-class");
+      navItems[i].classList.add("link__active");
+    } else {
+      sections[i].classList.remove("your-active-class");
+      navItems[i].classList.remove("link__active"); 
+    }
+  }
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
+// build the nav
+const buildNav = ()=> {
+  const nav = document.querySelector('#navbar__list');
+  const sections = document.querySelectorAll('section');
+  
+  sections.forEach(function(section) {
+    let navItem = createNavItem(section);
+    nav.appendChild(navItem);
+  });
+}
 
-initNav();
+// Add class 'active' to section when near top of viewport
+const scrollSectionActiveEvent = ()=> {
+  window.addEventListener('scroll', scrollEventHandler);
+}
+
+// Scroll to anchor ID using scrollTO event
+const scrollToSectionOnClick = ()=> {
+  const sections = document.querySelectorAll("section");
+  const navItems = document.querySelectorAll(".menu__link");
+
+  sections.forEach(function(section, index) {
+    if(0 == index) {
+      section.scrollIntoView ({behavior: 'smooth'});
+      navItems[index].classList.add('link__active');
+    }
+
+    navItems[index].addEventListener('click', function() {
+      // remove scroll event handler 
+      window.removeEventListener("scroll", scrollEventHandler);
+
+      // set the clicked navItem to active and all others to inactive
+      for(let navItem of navItems) {
+        navItem.classList.remove('link__active');
+      }
+      this.classList.add('link__active');
+
+      // scroll to the selected section
+      section.scrollIntoView ({behavior: 'smooth'});
+
+      // add scroll event handler back after 900ms
+      setTimeout(function() {
+        window.addEventListener("scroll", scrollEventHandler);
+      }, 900);
+    });
+  });
+}
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
+
+// Build menu 
+window.addEventListener("DOMContentLoaded", buildNav);
+// Scroll to section on link click
+window.addEventListener("DOMContentLoaded", scrollToSectionOnClick);
+// Set sections as active
+window.addEventListener("DOMContentLoaded", scrollSectionActiveEvent);
